58351a44f5eee3410e7d1323faec851a38674069
joebarker <joe.barker@othermedia.com>
2022-07-10 18:03:46 +0100

    Made basic launcher with fragments

---
+++ app/src/main/java/com/joebarker/haelauncher/AppLauncherFragment.kt
@@ -0,0 +1,7 @@
+package com.joebarker.haelauncher
+
+import androidx.fragment.app.Fragment
+
+class AppLauncherFragment : Fragment(R.layout.fragment_app_launcher) {
+
+}
---
+++ app/src/main/java/com/joebarker/haelauncher/HomeFragment.kt
@@ -0,0 +1,30 @@
+package com.joebarker.haelauncher
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.FragmentActivity
+import androidx.fragment.app.commit
+import androidx.fragment.app.replace
+import com.joebarker.haelauncher.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+
+    private lateinit var binding: FragmentHomeBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = FragmentHomeBinding.inflate(layoutInflater)
+        binding.appLauncherIcon.setOnClickListener { openAppLauncherFragment() }
+        return binding.root
+    }
+
+    private fun openAppLauncherFragment() {
+        parentFragmentManager.beginTransaction()
+        parentFragmentManager.commit {
+            setReorderingAllowed(true)
+            replace<AppLauncherFragment>(R.id.fragment_container)
+        }
+    }
+}
