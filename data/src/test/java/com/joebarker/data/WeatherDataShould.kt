191c85ac6cbe041625398db30de94c49802e3cbe
JoeGeC <joe.joeb29@gmail.com>
2022-07-11 22:48:44 +0100

    Updated gradle files

---
+++ data/src/test/java/com/joebarker/data/WeatherDataShould.kt
@@ -0,0 +1,11 @@
+package com.joebarker.data
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class WeatherDataShould {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2)
+    }
+}
