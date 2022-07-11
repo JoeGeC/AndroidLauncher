a218dc06e679c27341906c2e9f4d6f2172523956
JoeGeC <joe.joeb29@gmail.com>
2022-07-12 00:12:02 +0100

    Added test for unsuccessful api call

--- data/src/test/java/com/joebarker/data/WeatherDataShould.kt
+++ data/src/test/java/com/joebarker/data/WeatherDataShould.kt
@@ -1,17 +1,27 @@
 package com.joebarker.data
 
 import com.joebarker.domain.entities.WeatherInfo
-import org.junit.jupiter.api.Assertions.assertEquals
+import okhttp3.MediaType
+import okhttp3.ResponseBody
+import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 import org.mockito.kotlin.doReturn
 import org.mockito.kotlin.mock
+import org.mockito.kotlin.verify
+import retrofit2.Response
 import retrofit2.mock.Calls
+import java.lang.Exception
 
 class WeatherDataShould {
     private val expectedCity = "City"
     private val expectedCountry = "Country"
     private val expectedDescription = "Description description"
     private val expectedTemperature = 5
+    private val errorJson = "{\n" +
+            "    \"success\": false,\n" +
+            "    \"status_code\": 404,\n" +
+            "    \"status_message\": \"Not Found\"\n" +
+            "}"
 
     @Test
     fun retrieveWeatherInfoFromApi() {
@@ -23,6 +33,17 @@ class WeatherDataShould {
         val weatherData = WeatherDataImpl(remoteCalls)
         val expected = WeatherInfo(expectedCity, expectedCountry, expectedTemperature, expectedDescription)
         assertEquals(expected, weatherData.getWeatherInfoFor(passedInCity))
+    }
+
+    @Test
+    fun throwExceptionWhenFailure() {
+        val response = Response.error<WeatherInfoResponse>(404, ResponseBody.create(MediaType.parse("application/json"), errorJson))
+        val passedInCity = "city"
+        val remoteCalls = mock<WeatherRemoteCalls> {
+            on { retrieveWeatherInfo(passedInCity) } doReturn(Calls.response(response))
+        }
+        val weatherData = WeatherDataImpl(remoteCalls)
+        assertThrows(Exception::class.java) { weatherData.getWeatherInfoFor(passedInCity) }
     }
 
 }
